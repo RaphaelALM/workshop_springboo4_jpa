025363b3f615d6adad23017194af463adbaa1049
Raphael Almeida <almeida.phael57@gmail.com>
2026-06-01 14:21:43 +0900

    User entity and resource

---
+++ src/main/java/com/almeida/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.almeida.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.almeida.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Raphael" , "raphael@email.com", "12345678", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
