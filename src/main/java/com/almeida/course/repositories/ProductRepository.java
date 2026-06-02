e1abf0805616eaeda1bf1aec2c210f5389b895e9
Raphael Almeida <almeida.phael57@gmail.com>
2026-06-02 10:30:51 +0900

    Product

---
+++ src/main/java/com/almeida/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.almeida.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.almeida.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+
+}
