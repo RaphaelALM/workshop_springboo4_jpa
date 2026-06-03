251af448f47fa0ca825391ad971f853084cd8311
Raphael Almeida <almeida.phael57@gmail.com>
2026-06-03 09:13:03 +0900

    Exception handling - update

--- src/main/java/com/almeida/course/services/UserService.java
+++ src/main/java/com/almeida/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.almeida.course.repositories.UserRepository;
 import com.almeida.course.services.exception.DatabaseException;
 import com.almeida.course.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
